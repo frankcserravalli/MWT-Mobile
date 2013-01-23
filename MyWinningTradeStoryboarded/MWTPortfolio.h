1382a13aaefe8033119fb04d217091ec706f31a7
joelevin <joe.levin@gmail.com>
2013-01-23 11:57:06 -0500

    Added portfolio model

---
+++ MyWinningTradeStoryboarded/MWTPortfolio.h
@@ -0,0 +1,31 @@
+//
+//  MWTPortfolio.h
+//  MyWinningTradeStoryboarded
+//
+//  Created by Joseph Levin on 1/23/13.
+//  Copyright (c) 2013 Conclave Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MWTPortfolio : NSObject
+
+@property (nonatomic, strong) NSNumber *account_value;
+@property (nonatomic, strong) NSNumber *cash;
+@property (nonatomic, strong) NSNumber *current_value;
+@property (nonatomic, strong) NSNumber *percent_gain;
+@property (nonatomic, strong) NSNumber *purchase_value;
+
+@property (nonatomic, strong) NSDictionary *pending_date_time_transactions;
+@property (nonatomic, strong) NSDictionary *pending_stop_loss_transactions;
+@property (nonatomic, strong) NSDictionary *processed_date_time_transactions;
+@property (nonatomic, strong) NSDictionary *processed_stop_loss_transactions;
+
+@property (nonatomic, strong) NSDictionary *shorts;
+@property (nonatomic, strong) NSDictionary *stocks;
+
+- (void) getPortfolio;
+- (void) parsePortfolio:(NSData*)data;
+- (void) assignValuesFrom:(NSDictionary*)dictionary;
+
+@end
---
+++ MyWinningTradeStoryboarded/MWTPortfolio.m
@@ -0,0 +1,72 @@
+//
+//  MWTPortfolio.m
+//  MyWinningTradeStoryboarded
+//
+//  Created by Joseph Levin on 1/23/13.
+//  Copyright (c) 2013 Conclave Labs. All rights reserved.
+//
+
+#import "MWTPortfolio.h"
+#import "SBJson.h"
+
+@implementation MWTPortfolio
+
+- (id)init
+{
+    self = [super init];
+    
+    if (self)
+    {
+        [self getPortfolio];
+    }
+    
+    return self;
+}
+
+- (void) getPortfolio
+{
+    int user_id = 1;
+    
+    NSString *portfolioURLString = [NSString stringWithFormat:@"http://%@/api/v1/users/portfolio?user_id=%i", serverURL, user_id];
+    NSURL *portfolioURL = [NSURL URLWithString:portfolioURLString];
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:portfolioURL cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:60.0];
+    [request setHTTPMethod:@"GET"];
+   
+    NSError *requestError;
+    
+    NSURLResponse *urlResponse = nil;
+    NSData *response = [NSURLConnection sendSynchronousRequest:request returningResponse:&urlResponse error:&requestError];
+    
+    [self parsePortfolio:response];
+
+}
+
+- (void) parsePortfolio:(NSData *)data
+{
+    SBJsonParser *parser = [[SBJsonParser alloc] init];
+    NSDictionary *portfolio = [parser objectWithData:data];
+    
+    [self assignValuesFrom:portfolio];
+}
+
+- (void) assignValuesFrom:(NSDictionary *)dictionary
+{
+    _account_value = [dictionary objectForKey:@"account_value"];
+    _cash = [dictionary objectForKey:@"cash"];
+    _current_value = [dictionary objectForKey:@"current_value"];
+    _percent_gain = [dictionary objectForKey:@"percent_gain"];
+    _purchase_value = [dictionary objectForKey:@"purchase_value"];
+    
+    _pending_date_time_transactions = [dictionary objectForKey:@"pending_date_time_transactions"];
+    _processed_date_time_transactions = [dictionary objectForKey:@"processed_date_time_transactions"];
+    
+    _pending_stop_loss_transactions = [dictionary objectForKey:@"pending_stop_loss_transactions"];
+    _processed_stop_loss_transactions = [dictionary objectForKey:@"processed_stop_loss_transactions"];
+    
+    _shorts = [dictionary objectForKey:@"shorts"];
+    
+    _stocks = [dictionary objectForKey:@"stocks"];
+}
+
+@end
