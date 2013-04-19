b37b2b4b9a9f4b46b3bed158f69b3a7223a25578
joelevin <joe.levin@gmail.com>
2013-04-19 17:50:25 -0400

    UI adjustments

--- MyWinningTradeStoryboarded/MWTLoginViewController.h
+++ MyWinningTradeStoryboarded/MWTLoginViewController.h
@@ -18,4 +18,7 @@
 
 @property (strong, nonatomic) NSMutableData *responseData;
 
+@property float animatedDistance;
+
+
 @end
--- MyWinningTradeStoryboarded/MWTLoginViewController.m
+++ MyWinningTradeStoryboarded/MWTLoginViewController.m
@@ -16,14 +16,11 @@
 
 @implementation MWTLoginViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
+static const CGFloat KEYBOARD_ANIMATION_DURATION = 0.3;
+static const CGFloat MINIMUM_SCROLL_FRACTION = 0.2;
+static const CGFloat MAXIMUM_SCROLL_FRACTION = 0.8;
+static const CGFloat PORTRAIT_KEYBOARD_HEIGHT = 216;
+static const CGFloat LANDSCAPE_KEYBOARD_HEIGHT = 162;
 
 - (void)viewDidLoad
 {
@@ -47,6 +44,100 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void) viewWillAppear:(BOOL)animated
+{
+    [self enableKeyboardAppearanceNotifications];
+}
+
+- (void) enableKeyboardAppearanceNotifications
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardDidShow:) name:UIKeyboardDidShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardDidHide:) name:UIKeyboardDidHideNotification object:nil];
+}
+
+#pragma mark - UI Manipulation
+- (void) keyboardDidShow:(NSNotification *)notification
+{
+    NSLog(@"keyboard shown");
+    [self pushUpFrameWithTextfield:_emailTextfield];
+}
+
+- (void) keyboardDidHide:(NSNotification *)notification
+{
+    NSLog(@"keyboard hidden");
+    [self pushDownFrame];
+}
+
+
+- (void) pushUpFrameWithTextfield:(UITextField *)textField
+{
+    CGRect textFieldRect = [self.view.window convertRect:textField.bounds fromView:textField];
+    CGRect viewRect = [self.view.window convertRect:self.view.bounds fromView:self.view];
+    
+    CGFloat midline = textFieldRect.origin.y + 0.5 * textFieldRect.size.height;
+    CGFloat numerator = midline - viewRect.origin.y - MINIMUM_SCROLL_FRACTION * viewRect.size.height;
+    CGFloat denominator = (MAXIMUM_SCROLL_FRACTION - MINIMUM_SCROLL_FRACTION) * viewRect.size.height;
+    CGFloat heightFraction = numerator / denominator;
+    
+    if (heightFraction < 0.0)
+    {
+        heightFraction = 0.0;
+    }
+    else if (heightFraction > 1.0)
+    {
+        heightFraction = 1.0;
+    }
+    
+    UIInterfaceOrientation orientation = [[UIApplication sharedApplication] statusBarOrientation];
+    
+    if (orientation == UIInterfaceOrientationPortrait ||
+        orientation == UIInterfaceOrientationPortraitUpsideDown)
+    {
+        _animatedDistance = floor(PORTRAIT_KEYBOARD_HEIGHT * heightFraction);
+        
+    }
+    else
+    {
+        _animatedDistance = floor(LANDSCAPE_KEYBOARD_HEIGHT * heightFraction);
+    }
+    
+    CGRect viewFrame = self.view.frame;
+    viewFrame.origin.y -= _animatedDistance;
+    
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationBeginsFromCurrentState:YES];
+    [UIView setAnimationDuration:KEYBOARD_ANIMATION_DURATION];
+    
+    [self.view setFrame:viewFrame];
+    
+    [UIView commitAnimations];
+    
+}
+
+- (void) pushDownFrame
+{
+    
+    CGRect viewFrame = self.view.frame;
+    viewFrame.origin.y += _animatedDistance;
+    
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationBeginsFromCurrentState:YES];
+    [UIView setAnimationDuration:KEYBOARD_ANIMATION_DURATION];
+    
+    [self.view setFrame:viewFrame];
+    
+    [UIView commitAnimations];
+    
+}
+
+
+
+- (void) viewDidDisappear:(BOOL)animated
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
 
 - (IBAction)backgroundDismissKeyboard:(id)sender
 {
