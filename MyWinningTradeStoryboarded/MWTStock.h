200a11ae5d06eb1115a824428b3a8df35e664171
joelevin <joe.levin@gmail.com>
2013-05-09 18:45:48 -0400

    Refactored by decoupling fetching of data from model to controller

--- MyWinningTradeStoryboarded/MWTStock.h
+++ MyWinningTradeStoryboarded/MWTStock.h
@@ -9,9 +9,21 @@
 #import <Foundation/Foundation.h>
 @interface MWTStock : NSObject
 
-@property (strong, nonatomic) NSDictionary *stockDetails;
+//@property (strong, nonatomic) NSDictionary *stockDetails;
+//
+//-(void)getStockDetailsForStock:(NSString *)symbol;
+//-(void)parseStock:(NSData *)data;
 
--(void)getStockDetailsForStock:(NSString *)symbol;
--(void)parseStock:(NSData *)data;
+
+@property (strong, nonatomic) NSString *name;
+@property (strong, nonatomic) NSString *symbol;
+@property (strong, nonatomic) NSNumber *current_price;
+@property (strong, nonatomic) NSNumber *shares_owned;
+@property (strong, nonatomic) NSNumber *current_value;
+@property (strong, nonatomic) NSNumber *cost_basis;
+@property (strong, nonatomic) NSNumber *capital_gain;
+@property (strong, nonatomic) NSNumber *percent_gain;
+
+- (id) initWith:(NSDictionary *)dictionary;
 
 @end
--- MyWinningTradeStoryboarded/MWTStock.m
+++ MyWinningTradeStoryboarded/MWTStock.m
@@ -11,26 +11,44 @@
 @implementation MWTStock
 
 
-- (void) getStockDetailsForStock:(NSString *)symbol
-{    
-    NSString *stockDetailURLString = [NSString stringWithFormat:@"http://%@/api/v1/stocks/details?symbol=%@", serverURL, symbol];
-    NSURL *stockDetailURL = [NSURL URLWithString:stockDetailURLString];
-    
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:stockDetailURL cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:60.0];
-    [request setHTTPMethod:@"GET"];
-    NSError *requestError;
-    NSURLResponse *urlResponse = nil;
-    NSData *response = [NSURLConnection sendSynchronousRequest:request returningResponse:&urlResponse error:&requestError];
-    
-    [self parseStock:response];
-}
+//- (void) getStockDetailsForStock:(NSString *)symbol
+//{    
+//    NSString *stockDetailURLString = [NSString stringWithFormat:@"http://%@/api/v1/stocks/details?symbol=%@", serverURL, symbol];
+//    NSURL *stockDetailURL = [NSURL URLWithString:stockDetailURLString];
+//    
+//    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:stockDetailURL cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:60.0];
+//    [request setHTTPMethod:@"GET"];
+//    NSError *requestError;
+//    NSURLResponse *urlResponse = nil;
+//    NSData *response = [NSURLConnection sendSynchronousRequest:request returningResponse:&urlResponse error:&requestError];
+//    
+//    [self parseStock:response];
+//}
+//
+//- (void) parseStock:(NSData *)data
+//{
+//    NSError *error;
+//    NSDictionary *stockJSON = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:&error];
+//    NSDictionary *stockDetails = [stockJSON objectForKey:@"table"];
+//    _stockDetails = stockDetails;
+//}
 
-- (void) parseStock:(NSData *)data
+- (id) initWith:(NSDictionary *)dictionary
 {
-    NSError *error;
-    NSDictionary *stockJSON = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:&error];
-    NSDictionary *stockDetails = [stockJSON objectForKey:@"table"];
-    _stockDetails = stockDetails;
+    self = [super init];
+    
+    if (self)
+    {
+        _name = (NSString *)[dictionary objectForKey:@"name"];
+        _current_price = (NSNumber *)[dictionary objectForKey:@"current_price"];
+        _shares_owned = (NSNumber *)[dictionary objectForKey:@"shares_owned"];
+        _current_value = (NSNumber *)[dictionary objectForKey:@"current_value"];
+        _cost_basis = (NSNumber *)[dictionary objectForKey:@"cost_basis"];
+        _capital_gain = (NSNumber *)[dictionary objectForKey:@"capital_gain"];
+        _percent_gain = (NSNumber *)[dictionary objectForKey:@"percent_gain"];
+    }
+    
+    return self;
 }
 
 
