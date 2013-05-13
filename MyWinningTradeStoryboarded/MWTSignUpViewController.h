f2b90a919a833d0ab4d1e075896f24f4a80e7940
joelevin <joe.levin@gmail.com>
2013-05-13 17:51:21 -0400

    Begun work on the Sign Up view controller

--- MyWinningTradeStoryboarded/MWTSignUpViewController.h
+++ MyWinningTradeStoryboarded/MWTSignUpViewController.h
@@ -9,5 +9,14 @@
 #import <UIKit/UIKit.h>
 
 @interface MWTSignUpViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UITextField *username;
+@property (strong, nonatomic) IBOutlet UITextField *password;
+@property (strong, nonatomic) IBOutlet UITextField *confirmPassword;
+@property (strong, nonatomic) IBOutlet UIButton *submitButton;
+@property (strong, nonatomic) IBOutlet UIButton *cancelButton;
+
+- (IBAction)register:(id)sender;
+- (IBAction)cancel:(id)sender;
+
 
 @end
--- MyWinningTradeStoryboarded/MWTSignUpViewController.m
+++ MyWinningTradeStoryboarded/MWTSignUpViewController.m
@@ -35,4 +35,8 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)register:(id)sender {
+}
+- (IBAction)cancel:(id)sender {
+}
 @end
