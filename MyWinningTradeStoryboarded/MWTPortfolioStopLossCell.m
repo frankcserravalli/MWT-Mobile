6d1f5e912e249d48d31403d68ce15a4e09ec8b58
joelevin <joe.levin@gmail.com>
2013-05-13 17:49:23 -0400

    Added custom portfolio cell for stop loss positions

---
+++ MyWinningTradeStoryboarded/MWTPortfolioStopLossCell.m
@@ -0,0 +1,29 @@
+//
+//  MWTPortfolioStopLossCell.m
+//  MyWinningTradeStoryboarded
+//
+//  Created by Joseph Levin on 5/13/13.
+//  Copyright (c) 2013 Conclave Labs. All rights reserved.
+//
+
+#import "MWTPortfolioStopLossCell.h"
+
+@implementation MWTPortfolioStopLossCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
