6249a088ad21c5097986e58df7e4eecb792e0704
joelevin <joe.levin@gmail.com>
2013-05-13 17:48:37 -0400

    Added custom portfolio cell for Date Time positions

---
+++ MyWinningTradeStoryboarded/MWTPortfolioDateTimeCell.h
@@ -0,0 +1,14 @@
+//
+//  MWTPortfolioDateTimeCell.h
+//  MyWinningTradeStoryboarded
+//
+//  Created by Joseph Levin on 5/13/13.
+//  Copyright (c) 2013 Conclave Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MWTPortfolioDateTimeCell : UITableViewCell
+@property (strong, nonatomic) IBOutlet UILabel *cellTitle;
+
+@end
