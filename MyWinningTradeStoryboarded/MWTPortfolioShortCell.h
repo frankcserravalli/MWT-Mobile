d90098626c51cf696b3165976ce530230a0c850d
joelevin <joe.levin@gmail.com>
2013-05-13 17:49:04 -0400

    Added custom portfolio cell for shorted positions

---
+++ MyWinningTradeStoryboarded/MWTPortfolioShortCell.h
@@ -0,0 +1,14 @@
+//
+//  MWTPortfolioShortCell.h
+//  MyWinningTradeStoryboarded
+//
+//  Created by Joseph Levin on 5/13/13.
+//  Copyright (c) 2013 Conclave Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MWTPortfolioShortCell : UITableViewCell
+@property (strong, nonatomic) IBOutlet UILabel *cellTitle;
+
+@end
