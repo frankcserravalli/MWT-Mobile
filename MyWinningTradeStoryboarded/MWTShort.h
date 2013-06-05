793c9bb6e76602f2ee2cf0c13049290dc279f0c0
joelevin <joe.levin@gmail.com>
2013-06-05 17:29:24 -0400

    deprecating

--- MyWinningTradeStoryboarded/MWTShort.h
+++ MyWinningTradeStoryboarded/MWTShort.h
@@ -8,17 +8,17 @@
 
 #import <Foundation/Foundation.h>
 
-@interface MWTShort : NSObject
-
-@property (strong, nonatomic) NSString *capital_gain;
-@property (strong, nonatomic) NSString *cost_basis;
-@property (strong, nonatomic) NSString *current_price;
-@property (strong, nonatomic) NSString *current_value;
-@property (strong, nonatomic) NSString *name;
-@property (strong, nonatomic) NSNumber *percent_gain;
-@property (strong, nonatomic) NSNumber *shares_owned;
-@property (strong, nonatomic) NSString *symbol;
-
-- (id) initWith:(NSDictionary *)dictionary;
-
-@end
+//@interface MWTShort : NSObject
+//
+//@property (strong, nonatomic) NSString *capital_gain;
+//@property (strong, nonatomic) NSString *cost_basis;
+//@property (strong, nonatomic) NSString *current_price;
+//@property (strong, nonatomic) NSString *current_value;
+//@property (strong, nonatomic) NSString *name;
+//@property (strong, nonatomic) NSNumber *percent_gain;
+//@property (strong, nonatomic) NSNumber *shares_owned;
+//@property (strong, nonatomic) NSString *symbol;
+//
+//- (id) initWith:(NSDictionary *)dictionary;
+//
+//@end
--- MyWinningTradeStoryboarded/MWTShort.m
+++ MyWinningTradeStoryboarded/MWTShort.m
@@ -8,24 +8,24 @@
 
 #import "MWTShort.h"
 
-@implementation MWTShort
-
-- (id) initWith:(NSDictionary *)dictionary
-{
-    self = [super init];
-    
-    if (self)
-    {
-        _capital_gain = [dictionary objectForKey:@"capital_gain"];
-        _cost_basis = [dictionary objectForKey:@"cost_basis"];
-        _current_price = [dictionary objectForKey:@"current_price"];
-        _current_value = [dictionary objectForKey:@"current_value"];
-        _name = [dictionary objectForKey:@"name"];
-        _percent_gain = [dictionary objectForKey:@"percent_gain"];
-        _shares_owned = [dictionary objectForKey:@"shares_owned"];
-    }
-    
-    return self;
-}
-
-@end
+//@implementation MWTShort
+//
+//- (id) initWith:(NSDictionary *)dictionary
+//{
+//    self = [super init];
+//    
+//    if (self)
+//    {
+//        _capital_gain = [dictionary objectForKey:@"capital_gain"];
+//        _cost_basis = [dictionary objectForKey:@"cost_basis"];
+//        _current_price = [dictionary objectForKey:@"current_price"];
+//        _current_value = [dictionary objectForKey:@"current_value"];
+//        _name = [dictionary objectForKey:@"name"];
+//        _percent_gain = [dictionary objectForKey:@"percent_gain"];
+//        _shares_owned = [dictionary objectForKey:@"shares_owned"];
+//    }
+//    
+//    return self;
+//}
+//
+//@end
