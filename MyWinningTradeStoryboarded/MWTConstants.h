19707934a52ce16f8606e66c4e9e59ba14b9c07f
joelevin <joe.levin@gmail.com>
2013-06-05 17:39:58 -0400

    changed server to production

--- MyWinningTradeStoryboarded/MWTConstants.h
+++ MyWinningTradeStoryboarded/MWTConstants.h
@@ -1,2 +1,3 @@
 //#define serverURL @"localhost:3000"
-#define serverURL @"https://powerful-forest-8344.herokuapp.com"
+//#define serverURL @"https://powerful-forest-8344.herokuapp.com"
+#define serverURL @"https://mywinningtrade.com"
